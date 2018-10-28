b1ca7c942475198a0af90b66b27369080be5ec33
omcoin <38095962+oia-omcoin@users.noreply.github.com>
2018-10-29 10:08:27 +1100

    Create makeImg.sh

---
+++ makeImg.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+function createDmg {
+   NAME=$1
+
+    sudo macdeployqt ${NAME}.app
+    sudo chown -R omisaal ${NAME}.app/Contents/Frameworks/*
+    mkdir -p {NAME}
+    [ -d ${NAME}/${NAME}.app ] && rm -rf ${NAME}/${NAME}.app
+    cp -r ${NAME}.app ${NAME}/
+    rm tmp_${NAME}.dmg
+    hdiutil create tmp_${NAME}.dmg -srcfolder ${NAME}/
+    mkdir -p output
+    rm -rf output/*
+    hdiutil convert -format UDZO -o output/${NAME}.dmg tmp_${NAME}.dmg
+}
+
+createDmg $1
+
